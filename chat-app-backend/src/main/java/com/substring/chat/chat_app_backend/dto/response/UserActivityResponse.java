d0771751c3d54b8f5374c8cd8655b682c653d376
Phongfor <hoangphong2725@gmail.com>
2026-05-31 16:33:03 +0700

    feat: implement realtime room user presence tracking

--- chat-app-backend/src/main/java/com/substring/chat/chat_app_backend/dto/response/UserActivityResponse.java
+++ chat-app-backend/src/main/java/com/substring/chat/chat_app_backend/dto/response/UserActivityResponse.java
@@ -1,4 +1,17 @@
 package com.substring.chat.chat_app_backend.dto.response;
 
+import com.substring.chat.chat_app_backend.enums.ActivityType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class UserActivityResponse {
+    private String username;
+    private ActivityType type;
+    private List<String> onlineUsers;
 }
