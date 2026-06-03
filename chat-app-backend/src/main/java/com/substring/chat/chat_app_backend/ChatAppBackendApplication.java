926ba1f41c638d4cf21b3ffe3ad8a0f69a1757d0
Phongfor <hoangphong2725@gmail.com>
2026-06-03 16:38:30 +0700

    debug: log mongodb configuration on startup

--- chat-app-backend/src/main/java/com/substring/chat/chat_app_backend/ChatAppBackendApplication.java
+++ chat-app-backend/src/main/java/com/substring/chat/chat_app_backend/ChatAppBackendApplication.java
@@ -1,7 +1,10 @@
 package com.substring.chat.chat_app_backend;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChatAppBackendApplication {
@@ -10,4 +13,14 @@ public class ChatAppBackendApplication {
 		SpringApplication.run(ChatAppBackendApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner debugMongo(
+			@Value("${spring.data.mongodb.uri:NOT_FOUND}") String mongoUri) {
+		return args -> {
+			System.out.println("=================================");
+			System.out.println("SPRING_MONGO_URI = " + mongoUri);
+			System.out.println("ENV_MONGO_URL    = " + System.getenv("MONGO_URL"));
+			System.out.println("=================================");
+		};
+	}
 }
